f376d89bfd404e6a68c26c9f4be15f9c10e23487
Pedro-Augusto <augustopedroalves@gmail.com>
2023-05-23 23:09:14 -0300

    fix: arruma forma que divs dos novos to do são criadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ const selectTodoMobile = document.querySelector('.selectTodoMobile')
 const infTodoDesktop = document.querySelector('.infTodoDesktop')
 const itensLeft1 = document.querySelector('#itensLeft1')
 const itensLeft2 = document.querySelector('#itensLeft2')
+const newDivActive = document.querySelector('.new')
 
 let x = 0
 
@@ -17,6 +18,12 @@ input.addEventListener("keydown", function (event) {
     }
 })
 
+// newDivActive.addEventListener('click', activeTodoCircle)
+
+// function activeTodoCircle() {
+//     console.log('1')
+// }
+
 // criar uma funcao que consiga identificar que div é e fazera alteracao de classedo circulo para circulo ativo, alternando isso
 
 function createTodo() {
@@ -24,19 +31,43 @@ function createTodo() {
     let divNumber = x + 1
     x = divNumber
     
+
+    //cria div do novo todo e adiciona classe newTodo e id unica para a mesma
     const newDiv = document.createElement('div')
     newDiv.classList.add('newTodo')
-    
-    newDiv.innerHTML =
-    `
-    <div class="new" id="div${divNumber}">
+    newDiv.classList.add('active')
+    newDiv.setAttribute('id', 'div'+divNumber)
 
-        <div class="circle"></div>
-        <p>${input.value}</p>
-
-    </div>
-    `
     chores.appendChild(newDiv)
+
+    //cria div do circulo e adiciona classe circle para a mesma
+    let circleDiv = document.createElement('div')
+    circleDiv.classList.add('circle')
+    console.log(circleDiv) //apagar dps
+
+    //Diz que div do circulo é filha da div do novo todo
+    newDiv.appendChild(circleDiv)
+
+    //cria div do texto do todo e adiciona texto escrito por usuario para a mesma
+    let textDiv = document.createElement('p')
+    textDiv.innerHTML = input.value
+    console.log(textDiv) //apagar dps
+
+    //Diz que div do texto é filha da div do novo todo
+    newDiv.appendChild(textDiv)
+
+    circleDiv.addEventListener('click', ableDisable)
+    
+    // newDiv.innerHTML =
+    // `
+    // <div class="new" id="div${divNumber}">
+
+    //     <div class="circle"></div>
+    //     <p>${input.value}</p>
+
+    // </div>
+    // `
+    // chores.appendChild(newDiv)
     
     divCounter() //Conta números de divs e diz que input tem valor vazio para ser colocado outro to-do
 
@@ -45,6 +76,19 @@ function createTodo() {
 
     console.log('enter for apertado') //apagar dps  
     console.log(newDiv) //apagar dps
+}
+
+function ableDisable(e) {
+    let element = e.taget
+    if (element.classList.contains('active')) {
+        console.log('oi caralho')
+        element.classList.remove('active')
+        element.classList.add('disable')
+    } else {
+        console.log('aiinnnn')
+        element.classList.remove('disable')
+        element.classList.add('active')
+    }
 }
 
 function showInfoTodo() {
